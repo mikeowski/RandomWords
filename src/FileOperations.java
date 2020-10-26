1e64d622bdd75f751b271fb75189b77448181c8c
Burak Mike <burakmahyk@gmail.com>
2020-10-26 19:36:03 +0300

    txtEklendi

--- src/FileOperations.java
+++ src/FileOperations.java
@@ -3,8 +3,8 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class FileOperations {
-    public ArrayList<Word> getFile(){
-        File file = new File("words.txt");
-        if()
-    }
+    //public ArrayList<Word> getFile(){
+     //   File file = new File("words.txt");
+      //  if()
+    //}
 }
