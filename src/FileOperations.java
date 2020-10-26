0ad2f333f02bc4ac82479b73ed322010f5bb63f0
Burak Mike <burakmahyk@gmail.com>
2020-10-26 19:17:03 +0300

    FirstCommit

---
+++ src/FileOperations.java
@@ -0,0 +1,10 @@
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileOperations {
+    public ArrayList<Word> getFile(){
+        File file = new File("words.txt");
+        if()
+    }
+}
